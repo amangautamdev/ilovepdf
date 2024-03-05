246d92366a10d5fd8ce885ccffe59802a497da80
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-03-05 15:09:52 +0530

    Add files via upload

---
+++ ILovePDF/plugins/dm/callBack/file_process/__init__.py
@@ -0,0 +1,63 @@
+# This module is part of https://github.com/nabilanavab/ilovepdf
+# Feel free to use and contribute to this project. Your contributions are welcome!
+# copyright ©️ 2021 nabilanavab
+
+file_name = "ILovePDF/plugins/dm/callBack/file_process/__init__.py"
+
+__author__ = "amangautamdev"
+__email__ = "amangautam12006@gmail.com"
+__telegram__ = "telegram.dog/opamanxworld"
+__copyright__ = "Copyright 2021, amangautamdev"
+
+iLovePDF = '''
+  _   _                  ___  ___  ____ ™
+ | | | |   _____ _____  | _ \|   \|  __| 
+ | | | |__/ _ \ V / -_) |  _/| |) |  _|  
+ |_| |___,\___/\_/\___| |_|  |___/|_|    
+                         ❤ [M R 么 L E G E N D] 
+                         ❤ Email: amangautam12006@gmail.com
+                         ❤ Telegram: @LegendRobot
+'''
+
+from . import *
+
+__all__ = [
+    "previewPDF",
+    "textPDF",
+    "zoomPDF",
+    "formatPDF",
+    "encryptPDF",
+    "decryptPDF",
+    "combinePages",
+    "blackAndWhitePdf",
+    "compressPDF",
+    "decryptPDF",
+    "drawPDF",
+    "ocrPDF",
+    "rotatePDF",
+    "saturatePDF",
+    "pdfToImages",
+    "renamePDF",
+    "splitPDF",
+    "mergePDF",
+    "stampPDF",
+    "twoPagesToOne",
+    "threePagesToOne",
+    "threePagesToOneH",
+    "twoPagesToOneH",
+    "watermarkPDF",
+    "deletePDFPg",
+    "zipTarPDF",
+    "messagePDF",
+    "metadataPDF",
+    "watermark45",
+    "invertPDF",
+    "pdfFooter",
+    "pdfHeader",
+    "partPDF",
+    "urlRemover",
+]
+
+
+# If you have any questions or suggestions, please feel free to reach out.
+# Together, we can make this project even better, Happy coding!  XD
